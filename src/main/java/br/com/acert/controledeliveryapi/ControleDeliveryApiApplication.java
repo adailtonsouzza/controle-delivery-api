e23c55345d8fcbf13a0888dec0f90936a96cca5a
adailtonsouzza <97487447+adailtonsouzza@users.noreply.github.com>
2022-09-07 00:48:29 -0300

    criação do projeto

---
+++ src/main/java/br/com/acert/controledeliveryapi/ControleDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.acert.controledeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/acert/controledeliveryapi/ControleDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acert.controledeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
