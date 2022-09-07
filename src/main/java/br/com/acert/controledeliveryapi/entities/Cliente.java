32024d68b75827c771f1e06b4f405e140824d545
adailtonsouzza <97487447+adailtonsouzza@users.noreply.github.com>
2022-09-07 08:36:11 -0300

    criado um ambiente de teste e JPA implementado

--- src/main/java/br/com/acert/controledeliveryapi/entities/Cliente.java
+++ src/main/java/br/com/acert/controledeliveryapi/entities/Cliente.java
@@ -1,12 +1,18 @@
 package br.com.acert.controledeliveryapi.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@Entity
 public class Cliente implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String telefone;
